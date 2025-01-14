fcd96000c4cbf05fe1d8b90e6e3eb1c3997959be
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-01-14 00:17:09 -0800

    Update run_simulation_length_error.sh

--- Jan9/run_simulation_length_error.sh
+++ Jan9/run_simulation_length_error.sh
@@ -1,4 +1,11 @@
 python main.py --output_length_error_distance 0.1 --output_length_error_rate 0.1
 python main.py --output_length_error_distance 0.2 --output_length_error_rate 0.2
+python main.py --output_length_error_distance 0.3 --output_length_error_rate 0.3
 python main.py --output_length_error_distance 0.4 --output_length_error_rate 0.4
+python main.py --output_length_error_distance 0.5 --output_length_error_rate 0.5
 python main.py --output_length_error_distance 0.6 --output_length_error_rate 0.6
+python main.py --output_length_error_distance 0.7 --output_length_error_rate 0.7
+python main.py --output_length_error_distance 0.8 --output_length_error_rate 0.8
+python main.py --output_length_error_distance 0.9 --output_length_error_rate 0.9
+python main.py --output_length_error_distance 1.0 --output_length_error_rate 1.0
+

d27ab076755d005399cbcdd97b5cb70bba841f16
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-01-14 00:30:10 -0800

    Update run_simulation_latency.sh

--- Jan9/run_simulation_latency.sh
+++ Jan9/run_simulation_latency.sh
@@ -1,41 +1,72 @@
 # change of latency for predictor
 
-python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 2 --priority_predictor_batching_size 2
+python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode fully_delayed_processing
 
-python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 5 --priority_predictor_batching_size 5
+python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode immediate_processing
 
-python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 7 --priority_predictor_batching_size 7
+python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 5 --length_predictor_batching_size 5
 
-python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 10 --priority_predictor_batching_size 10
+python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode fully_delayed_processing
 
-python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 2 --priority_predictor_batching_size 2
+python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode immediate_processing
 
-python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 5 --priority_predictor_batching_size 5
+python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode fully_delayed_processing
 
-python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 7 --priority_predictor_batching_size 7
+python main.py  --priority_predictor_latency 0.1 --length_predictor_latency 0.1 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode immediate_processing
 
-python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 10 --priority_predictor_batching_size 10
+python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode fully_delayed_processing
 
-python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 2 --priority_predictor_batching_size 2
+python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode immediate_processing
 
-python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 5 --priority_predictor_batching_size 5
+python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 5 --length_predictor_batching_size 5
 
-python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 7 --priority_predictor_batching_size 7
+python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode fully_delayed_processing
 
-python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 10 --priority_predictor_batching_size 10
+python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode immediate_processing
 
-python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 2 --priority_predictor_batching_size 2
+python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode fully_delayed_processing
 
-python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 5 --priority_predictor_batching_size 5
+python main.py  --priority_predictor_latency 0.5 --length_predictor_latency 0.5 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode immediate_processing
 
-python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 7 --priority_predictor_batching_size 7
+python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode fully_delayed_processing
 
-python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 10 --priority_predictor_batching_size 10
+python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode immediate_processing
 
-python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 2 --priority_predictor_batching_size 2
+python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 5 --length_predictor_batching_size 5
 
-python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 5 --priority_predictor_batching_size 5
+python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode fully_delayed_processing
 
-python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 7 --priority_predictor_batching_size 7
+python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode immediate_processing
 
-python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 10 --priority_predictor_batching_size 10
+python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode fully_delayed_processing
+
+python main.py  --priority_predictor_latency 1 --length_predictor_latency 1 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode immediate_processing
+
+python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode fully_delayed_processing
+
+python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode immediate_processing
+
+python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 5 --length_predictor_batching_size 5
+
+python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode fully_delayed_processing
+
+python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode immediate_processing
+
+python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode fully_delayed_processing
+
+python main.py  --priority_predictor_latency 2 --length_predictor_latency 2 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode immediate_processing
+
+python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode fully_delayed_processing
+
+python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 2 --length_predictor_batching_size 2 --processing_mode immediate_processing
+
+python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 5 --length_predictor_batching_size 5
+
+python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode fully_delayed_processing
+
+python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 7 --length_predictor_batching_size 7 --processing_mode immediate_processing
+
+python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode fully_delayed_processing
+
+python main.py  --priority_predictor_latency 5 --length_predictor_latency 5 --priority_predictor_batching_size 10 --length_predictor_batching_size 10 --processing_mode immediate_processing
+

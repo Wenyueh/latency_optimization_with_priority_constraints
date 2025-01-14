4cf04a387156a31001f878aa5f1bc28e7a1d99b4
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-01-14 00:23:28 -0800

    Update semantic_predictor.py

--- Jan9/semantic_predictor.py
+++ Jan9/semantic_predictor.py
@@ -4,6 +4,7 @@ from utils import round_2
 import random, time
 import numpy as np
 from tqdm import tqdm
+from compute_arrival_time_utils import compute_arrival_time
 
 def initialize_user_request(args):
     user_request_priority = [random.choice(list(range(args.ranks))) for _ in range(args.user_request_num)]
@@ -11,26 +12,25 @@ def initialize_user_request(args):
     user_request_output_length = [random.choice(list(range(1, args.max_output_length))) for user_request in user_request_prompt_length]
     return user_request_priority, user_request_prompt_length, user_request_output_length
 
-
 # simulated oracle predictor
 def oracle_priority_predictor(args, user_request_ids):
     priorities = [config.user_request_priority[user_request_id] for user_request_id in user_request_ids]
     return priorities
 
 def oracle_output_length_bucket_predictor(args, user_request_ids):
-    r = random.randint(0,1)
-    if r == 1:
-        output_length_buckets = [
-            math.ceil(config.user_request_output_length[user_request_id] / 
-            (args.max_output_length/args.length_bucket_num))
-            for user_request_id in user_request_ids
-            ]
-    else:
-        output_length_buckets = [
-            math.floor(config.user_request_output_length[user_request_id] / 
-            (args.max_output_length/args.length_bucket_num))
-            for user_request_id in user_request_ids
-            ]
+    output_length_buckets = []
+    for user_request_id in user_request_ids:
+        r = random.randint(0,1)
+        if r == 1:
+            output_length_buckets.append(
+                math.ceil(config.user_request_output_length[user_request_id] / 
+                (args.max_output_length/args.length_bucket_num))
+                )
+        else:
+            output_length_buckets.append(
+                math.floor(config.user_request_output_length[user_request_id] / 
+                (args.max_output_length/args.length_bucket_num))
+                )
     return output_length_buckets
 
 # % of user requests that are mistaken by an error rate of priority_error_rate
@@ -40,7 +40,18 @@ def simulated_priority_predictor(args, priorities):
     mistaken_user_request_ids = random.sample(user_request_ids, int(len(user_request_ids)*args.priority_error_rate))
     for mistaken_user_request_id in mistaken_user_request_ids:
         error_distance = int(args.ranks * args.priority_error_distance)
-        mistaken_priority = min(args.ranks, max(1, config.user_request_priority[mistaken_user_request_id] + random.randint(-error_distance+1, error_distance+1)))
+        possibility1 = min(args.ranks, max(1, config.user_request_priority[mistaken_user_request_id] + error_distance))
+        possibility2 = min(args.ranks, max(1, config.user_request_priority[mistaken_user_request_id] -error_distance))
+        if possibility1 == priorities[mistaken_user_request_id]:
+            mistaken_priority = possibility2
+        elif possibility2 == priorities[mistaken_user_request_id]:
+            mistaken_priority = possibility1
+        else:
+            r = random.randint(0,1)
+            if r == 1:
+                mistaken_priority = possibility1
+            else:
+                mistaken_priority = possibility2
         priorities[mistaken_user_request_id] = mistaken_priority
     return priorities
 
@@ -48,8 +59,19 @@ def simulated_output_length_bucket_predictor(args, output_length_buckets):
     user_request_ids = list(range(len(output_length_buckets)))
     mistaken_user_request_ids = random.sample(user_request_ids, int(len(user_request_ids)*args.output_length_error_rate))
     for mistaken_user_request_id in mistaken_user_request_ids:
-        length_bucket_error_distance = int(args.length_bucket_num * args.output_length_error_distance)
-        mistaken_bucket = min(args.length_bucket_num, max(1, output_length_buckets[mistaken_user_request_id] + random.randint(-length_bucket_error_distance+1, length_bucket_error_distance+1)))
+        length_bucket_error_distance = int(args.length_bucket_num * args.output_length_error_distance) * (args.max_output_length/args.length_bucket_num)
+        possible1  = min(args.ranks, max(1, config.user_request_priority[mistaken_user_request_id] + length_bucket_error_distance))
+        possible2 = min(args.ranks, max(1, config.user_request_priority[mistaken_user_request_id] -length_bucket_error_distance))
+        if possible1 == output_length_buckets[mistaken_user_request_id]:
+            mistaken_bucket = possible2
+        elif possible2 == output_length_buckets[mistaken_user_request_id]:
+            mistaken_bucket = possible1
+        else:
+            r = random.randint(0,1)
+            if r == 1:
+                mistaken_bucket = possible1
+            else:
+                mistaken_bucket = possible2
         output_length_buckets[mistaken_user_request_id] = mistaken_bucket
     return output_length_buckets
 
@@ -75,7 +97,9 @@ def compute_generated_tokens(args, prompt_length, output_time):
     output_length = int(max(output_length_possibility_1, output_length_possibility_2))
     return output_length
 def compute_optimal_prefill_length(args, prompt_length):
-    return max(0,(args.cache_loading_speed - args.prefill_speed_coefficient2)/(2*args.prefill_speed_coefficient1*prompt_length)) 
+    numerator = args.cache_loading_speed - args.prefill_speed_coefficient2 * prompt_length
+    denominator = 2 * args.prefill_speed_coefficient1 * (prompt_length**2)
+    return min(max(0, numerator/denominator), 1)
 def compute_optimal_decoding_length(args, prompt_length):
     part1 = args.token_decoding_speed_coefficient1*prompt_length
     part2 = (args.token_decoding_speed_coefficient1 + 2*args.token_decoding_speed_coefficient2)/2
@@ -97,16 +121,6 @@ def compute_predicted_total_generation_time(args, user_request):
     total_generation_time = prefill_time + output_time
     return round_2([prefill_time, output_time, total_generation_time])
 
-# def cache_decode_or_recompute(args, output_length):
-#     cache_length = output_length - math.floor(args.cache_loading_speed/args.token_decoding_speed)
-#     recompute_length = output_length - cache_length
-#     return cache_length, recompute_length
-# def cache_prefill_or_not(args, prompt_length):
-#     if args.cache_loading_speed > args.prefill_speed * prompt_length:
-#         return False 
-#     else:
-#         return True
-
 # this is only called after the user request is preempted
 def update_cache_loading_or_recomputation_time_and_extra_saving_time(args, user_request):
     finished_computation_time = user_request.finished_computation_time
@@ -116,7 +130,7 @@ def update_cache_loading_or_recomputation_time_and_extra_saving_time(args, user_
 
     prompt_length = user_request.prompt_length
     prefill_time, _, _ = user_request.remaining_computation_time
-    
+
     # finish prefilling and now in decoding phase
     if prefill_time <= 0:
         ######## decoding update ########
@@ -187,38 +201,6 @@ def update_cache_loading_or_recomputation_time_and_extra_saving_time(args, user_
 
     return total_saving_cache_time
 
-# # this is only called after the user request is preempted
-# def compute_cache_saving_time(args, user_request):
-#     finished_computation_time = user_request.finished_computation_time
-#     prompt_length = user_request.prompt_length
-#     prefill_time, _, _ = user_request.predicted_remaining_computation_time
-#     # if we are in decoding phase
-#     if prefill_time == 0:
-#         # total generated tokens
-#         # compute the cache time for decoding
-#         generated_tokens = compute_generated_tokens(args, prompt_length, finished_computation_time-compute_prefill_time(args, prompt_length))
-#         total_cache_length = min(generated_tokens, user_request.optimal_decoding_cache_length)
-#         newly_saving_cache_length = min(total_cache_length-user_request.decoding_cache_length, 0)
-#         decoding_cache_time = newly_saving_cache_length * args.cache_saving_speed
-
-#         # compute cache time for prefilling
-#         if user_request.should_cache_prefill:
-#             prefilling_cache_time = (1 - user_request.prefill_cache_proportion) * prompt_length * args.cache_saving_speed
-#             user_request.prefill_cache_proportion = 1
-#             cache_time = round_2(prefilling_cache_time + decoding_cache_time)
-#             return cache_time
-#         else:
-#             return decoding_cache_time
-#     # if we are still in prefilling phase
-#     else:
-#         if user_request.should_cache_prefill:
-#             newly_saving_cache_proportion = finished_computation_time/compute_prefill_time(args, prompt_length) - user_request.prefill_cache_proportion
-#             user_request.prefill_cache_proportion = finished_computation_time/prefill_time
-#             return round_2(newly_saving_cache_proportion * prompt_length * args.cache_saving_speed)
-#         else:
-#             return 0
-
-
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--seed', type=int, default=0)
@@ -270,3 +252,4 @@ if __name__ == '__main__':
     simulated_predicted_output_bucket = simulated_output_length_bucket_predictor(args, list(range(args.user_request_num)), oracle_predicted_output_bucket)
     print(simulated_predicted_priority)
     print(simulated_predicted_output_bucket)
+

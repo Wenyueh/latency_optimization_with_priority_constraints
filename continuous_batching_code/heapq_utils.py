3134410db5c40615ba85dbcf6a121ddd7b836a30
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-03-12 16:19:30 -0400

    Update heapq_utils.py

--- continuous_batching_code/heapq_utils.py
+++ continuous_batching_code/heapq_utils.py
@@ -1,4 +1,5 @@
 import time 
+import heapq
 
 def _siftdown(heap, startpos, pos):
     newitem = heap[pos]
@@ -60,3 +61,15 @@ def smaller_than(a, b):
 #     if a[0] <= b[0]:
 #         return True 
 #     return False
+
+
+def heapify(x):
+    """Transform list into a heap, in-place, in O(len(x)) time."""
+    n = len(x)
+    # Transform bottom-up.  The largest index there's any point to looking at
+    # is the largest with a child index in-range, so must have 2*i + 1 < n,
+    # or i < (n-1)/2.  If n is even = 2*j, this is (2*j-1)/2 = j-1/2 so
+    # j-1 is the largest, which is n//2 - 1.  If n is odd = 2*j+1, this is
+    # (2*j+1-1)/2 = j so j-1 is the largest, and that's again n//2-1.
+    for i in reversed(range(n//2)):
+        _siftup(x, i)

ecae692d318adfbf43451f6be9471c95dfb77b1b
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-05-03 22:55:47 -0700

    Add files via upload

---
+++ continuous_batching_code/expriment_scripts/run_simulation_gap_A1007B.sh
@@ -0,0 +1,42 @@
+python main.py --user_request_gap 0.01 --max_concurrent_user_requests 5 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 0.01 --max_concurrent_user_requests 10 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 0.01 --max_concurrent_user_requests 20 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 0.01 --max_concurrent_user_requests 50 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 0.01 --max_concurrent_user_requests 100 --setting A100_7B --experiment_name gap
+
+
+python main.py --user_request_gap 0.1 --max_concurrent_user_requests 5 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 0.1 --max_concurrent_user_requests 10 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 0.1 --max_concurrent_user_requests 20 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 0.1 --max_concurrent_user_requests 50 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 0.1 --max_concurrent_user_requests 100 --setting A100_7B --experiment_name gap
+
+
+python main.py --user_request_gap 1 --max_concurrent_user_requests 5 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 1 --max_concurrent_user_requests 10 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 1 --max_concurrent_user_requests 20 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 1 --max_concurrent_user_requests 50 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 1 --max_concurrent_user_requests 100 --setting A100_7B --experiment_name gap
+
+
+python main.py --user_request_gap 5 --max_concurrent_user_requests 5 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 5 --max_concurrent_user_requests 10 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 5 --max_concurrent_user_requests 20 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 5 --max_concurrent_user_requests 50 --setting A100_7B --experiment_name gap
+
+python main.py --user_request_gap 5 --max_concurrent_user_requests 100 --setting A100_7B --experiment_name gap
---
+++ continuous_batching_code/expriment_scripts/run_simulation_gap_SPF.sh
@@ -0,0 +1,51 @@
+python SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 5 --setting A5000_7B_
+
+python SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 10 --setting A5000_7B_
+
+python SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 20 --setting A5000_7B_
+
+python SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 50 --setting A5000_7B_
+
+python SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 100 --setting A5000_7B_
+
+
+
+# python SPF_SPF_main.py --user_request_gap 0.99 --max_concurrent_user_requests 5
+
+# python SPF_SPF_main.py --user_request_gap 0.99 --max_concurrent_user_requests 10
+
+# python SPF_SPF_main.py --user_request_gap 0.99 --max_concurrent_user_requests 20
+
+# python SPF_SPF_main.py --user_request_gap 0.99 --max_concurrent_user_requests 50
+
+# python SPF_SPF_main.py --user_request_gap 0.99 --max_concurrent_user_requests 100
+
+# python SPF_SPF_main.py --user_request_gap 0.5 --max_concurrent_user_requests 5
+
+# python SPF_SPF_main.py --user_request_gap 0.5 --max_concurrent_user_requests 10
+
+# python SPF_SPF_main.py --user_request_gap 0.5 --max_concurrent_user_requests 20
+
+# python SPF_SPF_main.py --user_request_gap 0.5 --max_concurrent_user_requests 50
+
+# python SPF_SPF_main.py --user_request_gap 0.5 --max_concurrent_user_requests 100
+
+# python SPF_SPF_main.py --user_request_gap 0.2 --max_concurrent_user_requests 5
+
+# python SPF_SPF_main.py --user_request_gap 0.2 --max_concurrent_user_requests 10
+
+# python SPF_SPF_main.py --user_request_gap 0.2 --max_concurrent_user_requests 20
+
+# python SPF_SPF_main.py --user_request_gap 0.2 --max_concurrent_user_requests 50
+
+# python SPF_SPF_main.py --user_request_gap 0.2 --max_concurrent_user_requests 100
+
+# python SPF_SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 5
+
+# python SPF_SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 10
+
+# python SPF_SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 20
+
+# python SPF_SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 50
+
+# python SPF_SPF_main.py --user_request_gap 0.1 --max_concurrent_user_requests 100

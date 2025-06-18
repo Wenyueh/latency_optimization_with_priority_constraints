984e7ac089fd68cb534d2bfd82b4647675db7e9a
Wenyue Hua <28013619+Wenyueh@users.noreply.github.com>
2025-06-18 00:04:09 -0400

    Add files via upload

---
+++ experiment_scripts/run_simulation_length_error.sh
@@ -0,0 +1,11 @@
+python main.py --output_length_error_distance 0.1 --output_length_error_rate 0.1 --experiment_name "length"
+python main.py --output_length_error_distance 0.2 --output_length_error_rate 0.2 --experiment_name "length"
+python main.py --output_length_error_distance 0.3 --output_length_error_rate 0.3 --experiment_name "length"
+python main.py --output_length_error_distance 0.4 --output_length_error_rate 0.4 --experiment_name "length"
+python main.py --output_length_error_distance 0.5 --output_length_error_rate 0.5 --experiment_name "length"
+python main.py --output_length_error_distance 0.6 --output_length_error_rate 0.6 --experiment_name "length"
+python main.py --output_length_error_distance 0.7 --output_length_error_rate 0.7 --experiment_name "length"
+python main.py --output_length_error_distance 0.8 --output_length_error_rate 0.8 --experiment_name "length"
+python main.py --output_length_error_distance 0.9 --output_length_error_rate 0.9 --experiment_name "length"
+python main.py --output_length_error_distance 1.0 --output_length_error_rate 1.0 --experiment_name "length"
+
